69f6f6f83f8f59f6dbe9f9fa52e80c923d65dd45
Luan Reis <luannascimento.microcamp@gmail.com>
2025-06-30 16:24:05 -0300

    adicionando notação JPA

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,7 +1,11 @@
 package med.voll.api.medico;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import med.voll.api.endereco.Endereco;
 
+@Table(name ="medicos")
+@Entity(name ="Medico")
 public class Medico {
 
     private Long id;
