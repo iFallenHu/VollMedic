dd85afd0a781af2576d951f17bd6d1f0b6a25efe
Luan Reis <luannascimento.microcamp@gmail.com>
2025-06-30 16:35:19 -0300

    adicionando as notações JPA na classe Medico

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,17 +1,21 @@
 package med.voll.api.medico;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import med.voll.api.endereco.Endereco;
 
 @Table(name ="medicos")
 @Entity(name ="Medico")
 public class Medico {
 
+    @Id @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String crm;
+
+    @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
+
+    @Embedded
     private Endereco endereco;
 }
