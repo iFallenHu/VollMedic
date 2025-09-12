6d357ad853baecd10fdfcfd7ff4529d0177ec269
Luan Reis <luannascimento.microcamp@gmail.com>
2025-09-12 11:06:26 -0300

    aplicando metodo listar em pacientes

--- src/main/java/med/voll/api/paciente/PacienteController.java
+++ src/main/java/med/voll/api/paciente/PacienteController.java
@@ -2,11 +2,10 @@ package med.voll.api.paciente;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -20,5 +19,10 @@ public class PacienteController {
     public void cadastrar(@RequestBody  @Valid  DadosCadastroPaciente dados){
 
         pacienteRepository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente>listar (Pageable paginacao){
+        return pacienteRepository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
