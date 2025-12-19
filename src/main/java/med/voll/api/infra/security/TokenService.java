60f1920932846aaaa826569f59934e375ebfd560
Luan Reis <luannascimento.microcamp@gmail.com>
2025-12-19 15:42:09 -0300

    configurando permissões e liberando /login

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,7 @@ public class TokenService {
     private String secret;
 
     public String gerarToken(Usuario usuario){
-        System.out.println(secret);
+
         try {
             var algoritmo  = Algorithm.HMAC256(secret);
             return JWT.create()
@@ -30,6 +31,20 @@ public class TokenService {
             throw new RuntimeException("erro ao gerar token jwt", exception);
         }
     }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
     private Instant dataExpiracao(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
