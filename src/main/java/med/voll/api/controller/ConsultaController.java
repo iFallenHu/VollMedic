6bfe603c447642a5f5218bfd90109f9f00b85c82
Luan Reis <luannascimento.microcamp@gmail.com>
2026-01-23 16:06:51 -0300

    Validação de cancelamento consulta

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,14 +1,12 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
-import med.voll.api.domain.consulta.AgendaDeConsultas;
-import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
-import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import med.voll.api.domain.consulta.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("consultas")
@@ -20,8 +18,8 @@ public class ConsultaController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity agendar (@RequestBody @Valid DadosAgendamentoConsulta dados){
-       var dto = agenda.agendar(dados);
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        var dto = agenda.agendar(dados);
         return ResponseEntity.ok(dto);
     }
 
