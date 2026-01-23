f17078040d420a144a38b0eaa1c23b7972163806
Luan Reis <luannascimento.microcamp@gmail.com>
2026-01-23 14:34:39 -0300

    Adicionando validação para escolha de médico aleatório

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -40,6 +40,9 @@ public class AgendaDeConsultas {
 
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
         var medico = escolherMedico(dados);
+        if (medico == null){
+            throw new ValidacaoException("Não existe médico disponivel nessa data!");
+        }
         var consulta = new Consulta(null, medico, paciente, dados.data(), null);
         consultaRepository.save(consulta);
 
